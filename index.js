a17531eb9e501aef037906dcea9d74eafcfe3cf8
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-01-24 17:41:49 +0000

    requires added, some js written, base line layed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// INIT setup
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    name: "title",
+    type: 'input',
+    message: 'Enter title for project'
+  },
+  {
+    name: "description",
+    type: 'input',
+    message: 'Enter description'
+  },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data)
+}
+
+// function to initialize program
+function init() { 
+  inquirer.prompt(questions)
+    .then((answers) => {
+      const markdown = generateMarkdown(answers);
+      writeToFile('demoReadme.md', markdown)
+    })
+  
+}
+
+// function call to initialize program
+init();
